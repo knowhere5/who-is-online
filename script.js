ae2fd93d13f815b73d64ed94ca926b8b8660904f
Madhav <Madhavampire@gmail.com>
2024-01-14 00:32:50 +0530

    nice ui

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for anchor links (Optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
